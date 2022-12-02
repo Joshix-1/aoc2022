69e9b89fc77b42d9084c39ce407d29ef691366c0
Joshix-1 <joshix.dev@mailbox.org>
2022-12-02 22:18:52 +0100

    day1

---
+++ 01/main.py
@@ -0,0 +1,12 @@
+import sys
+from functools import reduce
+
+def main() -> None:
+    _ = tuple(sorted(
+        reduce(lambda x, y: x + y, map(int, filter(None, _.split("\n"))))
+        for _ in sys.stdin.read().split("\n\n") if _
+    ))[-3:]
+    print(f"1: {_[-1]}\n2: {sum(_)}")
+
+if __name__ == "__main__":
+    sys.exit(main())
