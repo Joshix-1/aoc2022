25031663fffdb56f98760db90880c2f2872307df
Joshix-1 <joshix.dev@mailbox.org>
2022-12-03 06:25:06 +0100

    day3 part2

--- 03/main.py
+++ 03/main.py
@@ -2,10 +2,13 @@
 
 import sys
 
+PRIO="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
 def main() -> None:
     in_ = sys.stdin.read()
     rucksacks = list(filter(None, in_.split("\n")))
     sum1 = 0
+    sum2 = 0
     for r in rucksacks:
         assert len(r) % 2 == 0
         middle = len(r) // 2
@@ -14,9 +17,15 @@ def main() -> None:
         union = set(first) & set(second)
         assert len(union) == 1
         letter = union.pop()
-        sum1 += 1 + "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".index(letter)
-
-    print(f"1: {sum1}\n2: ")
+        sum1 += 1 + PRIO.index(letter)
+    for i in range(len(rucksacks) // 3):
+        first, second, third = rucksacks[3 * i:3*i+3]
+        union = set(first) & set(second)
+        union &= set(third) & set(first)
+        assert len(union) == 1
+        letter = union.pop()
+        sum2 += 1 + PRIO.index(letter)
+    print(f"1: {sum1}\n2: {sum2}")
 
 if __name__ == "__main__":
     main()
