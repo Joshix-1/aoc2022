2cbec84f0389dc44ca3dcf0f9052c8d258808875
Joshix-1 <joshix.dev@mailbox.org>
2022-12-04 06:13:57 +0100

    day 4, part 1

---
+++ 04/main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import sys
+
+def elve_to_range(elve: str) -> list[int]:
+    start, end = elve.split("-")
+    return list(range(int(start), int(end) + 1))
+
+def main() -> None:
+    in_ = filter(None, sys.stdin.read().split("\n"))
+    count = 0
+    for line in in_:
+        elve1, elve2 = line.split(",")
+        r1, r2 = elve_to_range(elve1), elve_to_range(elve2)
+        if len(r1) == len(r2):
+            count += r1 == r2
+            continue
+        if len(r2) < len(r1):
+            r1, r2 = r2, r1
+        assert len(r1) < len(r2)
+        if r1[0] in r2 and r1[-1] in r2:
+            count += 1
+    print(f"1: {count}\n2:")
+
+if __name__ == "__main__":
+    main()
