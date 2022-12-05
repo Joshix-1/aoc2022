ac379423d163da65728fe7834a62b4b41813c6c1
Joshix-1 <joshix.dev@mailbox.org>
2022-12-05 06:19:04 +0100

    day 5, part 2

--- 05/main.py
+++ 05/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import sys
 
-def solve(input_: str) -> tuple[str, int]:
+def solve(input_: str) -> tuple[str, str]:
     initial, moves = list(filter(None, input_.split("\n\n")))
 
     crate_lines = list(filter(None, initial.split("\n")))
@@ -15,16 +15,20 @@ def solve(input_: str) -> tuple[str, int]:
             if line[i] == " ":
                 continue
             crates[idx].append(line[i])
+    crates2 = {key: list(value) for key, value in crates.items()}
     print(crates)
     lines = list(filter(None, moves.split("\n")))
-    res2 = 0
 
     for line in lines:
         _, count, _, from_, _, to_ = line.split(" ")
         count, from_, to_ = int(count), int(from_), int(to_)
+        crates2[to_].extend(crates2[from_][-count:])
         for _ in range(count):
+            crates2[from_].pop()
             crates[to_].append(crates[from_].pop())
+
     res1 = "".join(stack[-1] for stack in crates.values())
+    res2 = "".join(stack[-1] for stack in crates2.values())
     return res1, res2
 
 def main() -> None:
