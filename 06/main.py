67f50e8f810a594e35dc66eec782aa7872513a60
Joshix-1 <joshix.dev@mailbox.org>
2022-12-06 06:04:56 +0100

    day 6, part 1

---
+++ 06/main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import sys
+
+def solve(input_: str) -> tuple[int | str, int | str]:
+    chars: list[str] = list(filter(None, input_.strip()))
+
+    res1, res2 = 0, 0
+
+    for i in range(len(chars) - 4):
+        ch = chars[i]
+        _4 = chars[i:i+4]
+        print(f"{res1=}, {res2=}, {ch=}, {_4=}")
+        if len(set(_4)) == 4:
+            res1 = i+4
+            break
+
+    return res1, res2
+
+def main() -> None:
+    stdout, sys.stdout = sys.stdout, sys.stderr
+    try:
+        res1, res2 = solve(sys.stdin.read())
+    finally:
+        sys.stdout = stdout
+    print(f"1: {res1}\n2: {res2}")
+
+if __name__ == "__main__":
+    sys.exit(main())
