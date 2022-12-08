75e5483fdfb077c0222df587f11d76bf06c65211
Joshix-1 <joshix.dev@mailbox.org>
2022-12-08 07:00:05 +0100

    day 8, part 2 (failed)

--- 08/main.py
+++ 08/main.py
@@ -4,16 +4,19 @@ import sys
 def solve(input_: str) -> tuple[int | str, int | str]:
     lines: list[str] = list(filter(None, input_.split("\n")))
 
-    res1, res2 = 0, 0
+    res1 = 0
 
 
     tree_grid = [list(map(int, line)) for line in lines]
     print(tree_grid)
     visibility = [[False] * len(lines[0])] * len(lines)
 
+    senic_scores = []
+
     for y, line in enumerate(tree_grid):
         for x, tree in enumerate(line):
             vis = False
+            cols = [row[x] for row in tree_grid]
             if y in {0, len(tree_grid) - 1}:
                 vis = True
             elif x in {0, len(line) - 1}:
@@ -23,12 +26,38 @@ def solve(input_: str) -> tuple[int | str, int | str]:
             elif max(line[x + 1:]) < tree:
                 vis = True
             else:
-                cols = [row[x] for row in tree_grid]
                 vis = max(cols[:y]) < tree or max(cols[y + 1:]) < tree
             res1 += vis
             print(f"{x=}, {y=}, {vis=}, {tree=!r}, {res1=}")
 
-    return res1, res2
+            x1 = 1
+            for x_ in range(x + 1, len(line)):
+                assert x_ != x
+                if line[x_] >= tree:
+                    break
+                x1 += 1
+            
+            x2 = 1
+            for x_ in reversed(list(range(x))):
+                assert x_ != x
+                if line[x_] >= tree:
+                    break
+                x2 += 1
+            y1 = 1
+            for y_ in range(y + 1, len(cols)):
+                assert y_ != y
+                if cols[y_] >= tree:
+                    break
+                y1 += 1
+            y2 = 1
+            for y_ in reversed(list(range(y))):
+                assert y_ != y
+                if cols[y_] >= tree:
+                    break
+                y2 += 1
+            senic_scores.append(x1 * x2 * y1 * y2)
+
+    return res1, max(senic_scores)
 
 def main() -> None:
     stdout, sys.stdout = sys.stdout, sys.stderr
