4599daec66b171bbc5e41d0cb1725c98763b46f9
Joshix-1 <joshix.dev@mailbox.org>
2022-12-10 06:36:41 +0100

    day 10, p2

--- 10/main.py
+++ 10/main.py
@@ -5,6 +5,13 @@ def print_crt(crt):
     for l in crt:
         print("".join(l))
 
+def draw(crt, x, cycle):
+    crt_x = (cycle - 1) % 40
+    lit = crt_x in (x - 1, x, x + 1)
+    cycle = (cycle - 1) % 240
+    print((cycle) // 40, crt_x)
+    crt[(cycle) // 40][crt_x] = "#" if lit else "."
+
 def solve(input_: str) -> tuple[int | str, int | str]:
     lines: list[str] = list(filter(None, input_.split("\n")))
 
@@ -12,11 +19,15 @@ def solve(input_: str) -> tuple[int | str, int | str]:
 
     x = 1
 
-    crt = [["#"] * 40] * 6
-
+    crt = [["-"] * 40] * 6
+    for i in range(len(crt)):
+        crt[i] = list(tuple(crt[i]))
+    sprite_w = 3
+    
     cycle = 1
     for line in lines:
         print(f"{cycle=}, {x=}, {line=}")
+        draw(crt, x, cycle)
         print_crt(crt)
         if cycle in {20, 60, 100, 140, 180, 220}:
             res1 += cycle * x
@@ -25,12 +36,13 @@ def solve(input_: str) -> tuple[int | str, int | str]:
         else:
             assert line.startswith("addx ")
             cycle += 1
+            draw(crt, x, cycle)
             if cycle in {20, 60, 100, 140, 180, 220}:
                 res1 += cycle * x
             cycle += 1
             x += int(line.removeprefix("addx "))
 
-    return res1, res2
+    return res1, crt
 
 def main() -> None:
     stdout, sys.stdout = sys.stdout, sys.stderr
@@ -38,7 +50,8 @@ def main() -> None:
         res1, res2 = solve(sys.stdin.read())
     finally:
         sys.stdout = stdout
-    print(f"1: {res1}\n2: {res2}")
+    print(f"1: {res1}\n2:")
+    print_crt(res2)
 
 if __name__ == "__main__":
     sys.exit(main())
