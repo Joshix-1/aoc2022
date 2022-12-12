27103dc786950bde6d9f972eadc32a7025e604f8
Joshix-1 <57299889+Joshix-1@users.noreply.github.com>
2022-12-12 14:27:50 +0100

    Update main.py

--- 12/main.py
+++ 12/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import sys
-
+from dataclasses import dataclass
+sys.setrecursionlimit(16385)
 def get_next(i, x, y):
     if i == 0:
         return (x + 1, y)
@@ -12,51 +13,63 @@ def get_next(i, x, y):
         return (x, y - 1)
     assert False
 
+@dataclass
+class Cell:
+    char: str
+    dist: int = None
+
 def solve_maze(maze: "list[list[str]]", x, y, ev: str) -> int:
     if min(x, y) < 0 or x >= len(maze[0]) or y >= len(maze):
-        return 0
-    #for line in maze:
-    #    print("".join(line))
-    curr = maze[y][x]
+        return None
+    curr = maze[y][x].char
     if curr == "E":
+        maze[y][x].dist = 0
         return int(ev in {"z", "y"})
-    if curr in ".<>^V":
-        return 0
-    if curr != "S":
+    if curr == ".":
+        return None
+    if curr != "S" and curr not in "<>V^":
         diff = ord(curr) - ord(ev)
         if diff > 1:
             #print(diff, ev, curr)
-            return 0
+            return None
     # can go to this
-    maze[y][x] = "."
+    maze[y][x] = Cell(".")
     ret = [0, 0, 0, 0]
-    for i in range(4):
+    for i in reversed(range(4)):
         ret[i] = solve_maze(
             maze,
             *get_next(i,x,y),
             curr if curr != "S" else "a"
         )
-    ret_ = list(filter(None, ret))
+    ret_ = list(filter(lambda x: x is not None, ret))
     if ret_:
-        maze[y][x] = ">^<V"[ret.index(min(ret_))]
+        maze[y][x].dist = min(ret_) + 1
+        maze[y][x].char = ">V<^"[ret.index(min(ret_))]
         return min(ret_) + 1
-    maze[y][x] = curr
-    return 0
+    return maze[y][x].dist
 
-def solve(input_: str) -> tuple[int | str, int | str]:
+def solve(input_: str) -> "tuple[int | str, int | str]":
     lines: list[str] = list(filter(None, input_.split("\n")))
 
     res1, res2 = 0, 0
     assert lines[0][0] == "S" or lines[20][0] == "S"
+    maze = [[Cell(c) for c in x] for x in lines]
     res1 = solve_maze(
-        [list(x) for x in lines],
+        maze,
         0,
         0 if lines[0][0] == "S" else 20,
         "a"
-    ) - 1
-    for line in lines:
+    )
+    res1 = solve_maze(
+        maze,
+        0,
+        0 if lines[0][0] == "S" else 20,
+        "a"
+    )
+
+    for line in maze:
+        print("".join([c.char for c in line]))
         pass # print(f"{res1=}, {res2=}, {line=}")
-        
 
     return res1, res2
 
