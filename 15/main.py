55886b75f3a675600cc9ff679a28205f82da3d24
Joshix-1 <57299889+Joshix-1@users.noreply.github.com>
2022-12-15 09:18:54 +0100

    Update main.py

--- 15/main.py
+++ 15/main.py
@@ -3,13 +3,13 @@ import re
 import sys
 
 
-def manhattan_dist(a: tuple[int, int], b: tuple[int, int]) -> int:
+def manhattan_dist(a: "tuple[int, int]", b: "tuple[int, int]") -> int:
     return abs(a[0] - b[0]) + abs(a[1] - b[1])
 
 
 class Sensor:
-    sensor: tuple[int, int]
-    beacon: tuple[int, int]
+    sensor: "tuple[int, int]"
+    beacon: "tuple[int, int]"
     distance: int
 
     def __init__(self, line: str) -> None:
@@ -25,7 +25,7 @@ class Sensor:
     def __str__(self):
         return f"S{self.sensor}, dist={self.distance}"
 
-    def covered_pos(self, y: int) -> set[tuple[int, int]]:
+    def covered_pos(self, y: int) -> "set[tuple[int, int]]":
         covered = set()
         x = self.sensor[0]
         while True:
@@ -36,7 +36,7 @@ class Sensor:
                 x += 1
             else:
                 break
-        x = self.sensor[0]
+        x = self.sensor[0] - 1
         while True:
             pos = (x, y)
             distance = manhattan_dist(pos, self.sensor)
@@ -48,6 +48,31 @@ class Sensor:
 
         return covered
 
+    def covered_pos_range(self, y: int, start: int, end: int) -> "set[tuple[int, int]]":
+        covered = set()
+        x = self.sensor[0]
+        while x <= end:
+            pos = (x, y)
+            distance = manhattan_dist(pos, self.sensor)
+            if distance <= self.distance:
+                covered.add(pos)
+                x += 1
+            else:
+                break
+        x = self.sensor[0] - 1
+        while start <= x:
+            pos = (x, y)
+            distance = manhattan_dist(pos, self.sensor)
+            if distance <= self.distance:
+                covered.add(pos)
+                x -= 1
+            else:
+                break
+
+        return covered
+
+def pos_in_range(pos: "tuple[int, int]", start, end) -> bool:
+    return start <= pos[0] <= end and start <= pos[1] <= end
 
 def solve(input_: str) -> "tuple[int | str, int | str]":
     lines: list[str] = list(filter(None, input_.split("\n")))
@@ -58,30 +83,30 @@ def solve(input_: str) -> "tuple[int | str, int | str]":
         Sensor(line)
         for line in lines
     ]
-    covered_pos: set[tuple[int, int]] = set()
-    y = 200_0000
+    covered_pos: "set[tuple[int, int]]" = set()
+    y = 10 if "test" in sys.argv else 200_0000
     for sensor in sensors:
-        print(f"{sensor=!s}")
+        print(f"{sensor!s}")
         covered_pos |= sensor.covered_pos(y)
-
-    for x in range(4000000 + 1):
-        for y in range(4000000 + 1):
-            covered = False
-            for sensor in sensors:
-                if manhattan_dist((x, y), sensor.sensor) <= sensor.distance:
-                    covered = True
-                    break
-            if not covered:
-                res2.append((x, y))
-                break
-        if res2:
+    max_pos = 20 if "test" in sys.argv else 4_000_000
+    for y in range(0, max_pos + 1):
+        if not y % 100_000:
+            print(y)
+        covered = set()
+        for sensor in sensors:
+            covered |= sensor.covered_pos_range(y, 0, max_pos)
+        if len(covered) != max_pos + 1:
+            print(f"{y}, {len(covered)}")
             break
-
+    for x in range(max_pos + 1):
+        if (x, y) not in covered:
+            res2.append((x, y))
+            break
     for sensor in sensors:
         if sensor.beacon in covered_pos:
             covered_pos.remove(sensor.beacon)
 
-    print(f"{res2=}")
+    print(f"{res2}")
     return len(covered_pos), res2[0][0] * 4000000 + res2[0][1]
 
 
