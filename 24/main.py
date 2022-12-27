2b736f488848eb3d7836420b0028c0f58e5a80ce
Joshix-1 <joshix.dev@mailbox.org>
2022-12-27 20:57:07 +0100

    d24, fix for test input

--- 24/main.py
+++ 24/main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env pypy3
 import sys
+from collections.abc import Iterable
 from typing import NamedTuple
 
 class Blizzard:
@@ -12,17 +13,20 @@ class Blizzard:
     def get_pos_at(
         self, minute: int, width: int, height: int
     ) -> tuple[int, int]:
-        # if minute in self.cache:
-        #     return self.cache[minute]
-        x, y = self.pos
+        if minute == 0:
+            return self.pos
+        assert minute > 0
+        if minute in self.cache:
+            return self.cache[minute]
+        x, y = self.get_pos_at(minute - 1, width, height)
         if self.char in {"v", "^"}:
-            y += minute * (-1 if self.char == "^" else 1)
+            y += -1 if self.char == "^" else 1
             if y < 1:
                 y = height - 2
             elif y >= height - 1:
                 y = 1
         if self.char in {"<", ">"}:
-            x += minute * (-1 if self.char == "<" else 1)
+            x += -1 if self.char == "<" else 1
             if x < 1:
                 x = width - 2
             elif x >= width - 1:
@@ -35,25 +39,28 @@ class State(NamedTuple):
     minute: int
     x: int
     y: int
-    visited: list[tuple[int, int]]
+    visited: tuple[tuple[int, int], ...]
 
-    def get_next_moves(self) -> "list[State]":
+    @property
+    def pos(self) -> tuple[int, int]:
+        return self.x, self.y
+
+    def get_next_moves(self) -> "Iterable[State]":
         minute = self.minute + 1
-        visited = self.visited + [(self.x, self.y)]
-        states: "list[State]" = []
-        for pos in [
-            (self.x + 1, self.y),
-            (self.x, self.y + 1),
-            (self.x - 1, self.y),
-            (self.x, self.y - 1),
-            (self.x, self.y),
-        ]:
-            if pos in visited and visited.count(pos) > 100:
-                continue
-            if min(pos) < 0:
-                continue
-            states.append(State(minute, *pos, visited=list(visited)))
-        return states
+        curr_pos = x, y = self.pos
+        visited = self.visited[-50:] + (curr_pos,)
+        return (
+            State(minute, *pos, visited=tuple(visited))
+            for pos in [
+                (x + 1, y),
+                (x, y + 1),
+                (x - 1, y),
+                (x, y - 1),
+                curr_pos,
+            ]
+            if pos not in visited or visited.count(pos) < 10
+            if min(pos) >= 0
+        )
 
 def test():
     pass
@@ -108,28 +115,37 @@ def solve(input_: str) -> "tuple[int | str, int | str]":
                 #maze[y][x] = "."
 
     res1 = -1
-    moves: list[State] = [State(0, pos[0], pos[1], [])]
-    blizz_pos_lists: dict[int, tuple[tuple[int, int], ...]] = {}
-    while moves:
-        moves.sort(key=lambda m: (m.minute, -m.x, -m.y))
-        move = moves.pop(0)
-        minute, x, y, visited = move
-        if (x, y) == exi:
-            res1 = minute
-            break
-        if maze[y][x] == "#":
-            continue
-        blizz_pos_tpl = blizz_pos_lists[minute] if minute in blizz_pos_lists else tuple(
-            blizz.get_pos_at(minute, width, height)
-            for blizz in blizzards
-        )
-        if (x, y) in blizz_pos_tpl:
-            continue
-        # print_m_b(maze, blizzards, minute, (x, y))
-        moves.extend(move.get_next_moves())
+    moves: list[State] = [State(0, pos[0], pos[1], ())]
+    blizz_pos_lists: dict[int, set[tuple[int, int]]] = {}
 
-    for m in range(minute):
-        print_m_b(maze, blizzards, m, None)
+    def get_blizz_pos_list(_min: int) -> set[tuple[int, int]]:
+        if _min not in blizz_pos_lists:
+            blizz_pos_lists[_min] = set(
+                blizz.get_pos_at(_min, width, height)
+                for blizz in blizzards
+            )
+        return blizz_pos_lists[_min]
+
+    last_minute = 0
+    while moves:
+        # moves.sort(key=lambda m: (m.minute, -m.x, -m.y))
+        move = moves.pop(0)
+        if move.pos == exi:
+            res1 = move.minute
+            break
+        if move.minute > last_minute:
+            last_minute = move.minute
+            print(move.minute, "moves:", len(moves))
+        # print_m_b(maze, blizzards, minute, (x, y))
+        moves.extend(
+            m
+            for m in move.get_next_moves()
+            if m.pos not in get_blizz_pos_list(m.minute)
+            if maze[m.y][m.x] != "#"
+        )
+
+    # for m in range(minute):
+    #     print_m_b(maze, blizzards, m, None)
 
     return res1, -1
 
