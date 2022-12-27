56853a794d42c97190cb6c5338ea3137f9bb1378
Joshix-1 <joshix.dev@mailbox.org>
2022-12-27 17:41:08 +0100

    d24, better printing

--- 24/main.py
+++ 24/main.py
@@ -41,7 +41,13 @@ class State(NamedTuple):
         minute = self.minute + 1
         visited = self.visited + [(self.x, self.y)]
         states: "list[State]" = []
-        for pos in [(self.x + 1, self.y), (self.x, self.y + 1), (self.x - 1, self.y), (self.x, self.y - 1), (self.x, self.y)]:
+        for pos in [
+            (self.x + 1, self.y),
+            (self.x, self.y + 1),
+            (self.x - 1, self.y),
+            (self.x, self.y - 1),
+            (self.x, self.y),
+        ]:
             if pos in visited and visited.count(pos) > 100:
                 continue
             if min(pos) < 0:
@@ -49,6 +55,45 @@ class State(NamedTuple):
             states.append(State(minute, *pos, visited=list(visited)))
         return states
 
+def test():
+    pass
+
+def print_m_b(
+    maze: list[list[str]],
+    blizzards: list[Blizzard],
+    minute: int,
+    pos: "tuple[int, int] | None"
+):
+    width, height = len(maze[0]), len(maze)
+    maze: list[list[str]] = [
+        [cell for cell in row]
+        for row in maze
+    ]
+    blizz_pos = {
+        blizz.get_pos_at(minute, width, height): blizz
+        for blizz in blizzards
+    }
+    blizz_pos_list = [
+        blizz.get_pos_at(minute, width, height)
+        for blizz in blizzards
+    ]
+    print(f"--- Minute {minute} ---")
+    for y in range(height):
+        for x in range(width):
+            if (x, y) in blizz_pos:
+                count = blizz_pos_list.count((x, y))
+                print(
+                    blizz_pos[(x, y)].char
+                    if count == 1
+                    else (count if count < 10 else "x"),
+                    end="",
+                    flush=False
+                )
+            elif maze[y][x] == "#":
+                print("#", end="", flush=False)
+            else:
+                print(" " if pos != (x, y) else "E", end="", flush=False)
+        print()
 
 def solve(input_: str) -> "tuple[int | str, int | str]":
     maze: list[list[str]] = list(map(list, filter(None, input_.split("\n"))))
@@ -60,29 +105,31 @@ def solve(input_: str) -> "tuple[int | str, int | str]":
         for x in range(width):
             if maze[y][x] in {"v", "^", ">", "<"}:
                 blizzards.append(Blizzard(maze[y][x], x=x, y=y))
-                maze[y][x] = "."
+                #maze[y][x] = "."
 
     res1 = -1
     moves: list[State] = [State(0, pos[0], pos[1], [])]
+    blizz_pos_lists: dict[int, tuple[tuple[int, int], ...]] = {}
     while moves:
-        moves.sort(key=lambda m: (m[0], -m[1], -m[2]))
+        moves.sort(key=lambda m: (m.minute, -m.x, -m.y))
         move = moves.pop(0)
         minute, x, y, visited = move
         if (x, y) == exi:
             res1 = minute
-            print(minute)
             break
         if maze[y][x] == "#":
             continue
-        if any(
-            True
+        blizz_pos_tpl = blizz_pos_lists[minute] if minute in blizz_pos_lists else tuple(
+            blizz.get_pos_at(minute, width, height)
             for blizz in blizzards
-            if blizz.get_pos_at(minute, width, height) == (x, y)
-        ):
-            print(minute, (x, y), "blizz")
+        )
+        if (x, y) in blizz_pos_tpl:
             continue
-        print(move)
+        # print_m_b(maze, blizzards, minute, (x, y))
         moves.extend(move.get_next_moves())
+
+    for m in range(minute):
+        print_m_b(maze, blizzards, m, None)
 
     return res1, -1
 
