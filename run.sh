c07fb744d6a9e8deb34448ef1edc25746b7f18a1
Joshix-1 <joshix.dev@mailbox.org>
2023-01-03 09:20:40 +0100

    improve run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
-if [ ! -d $1 ]; then
-    cp -r 00 $1
+
+if [ ! -d "$1" ]; then
+  echo "Creating dir $1 from template."
+  cp -r 00 "$1"
 fi
-env PYPY_GC_MAX=4G ./$1/main.py < ./$1/input
+
+INPUT_FILE="./$1/input"
+
+if [ "$2" = "test" ]; then
+  INPUT_FILE="./$1/test-input"
+elif [ -n "$2" ]; then
+  echo "Invalid second argument '$2'"
+  exit 1
+fi
+
+if [ ! -f "$INPUT_FILE" ]; then
+  echo "'$INPUT_FILE' doesn't exist!"
+  exit 2
+fi
+exec env PYPY_GC_MAX=4G "./$1/main.py" < "$INPUT_FILE"
